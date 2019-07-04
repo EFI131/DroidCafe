00a2ea36ae6446dbf023b62505316bc3153b05e3
EFI131 <efi131@gmail.com>
2019-07-05 04:09:43 +0800

    Added editTexts & radioGroup

--- app/src/main/java/com/com/droidcafe/OrderActivity.java
+++ app/src/main/java/com/com/droidcafe/OrderActivity.java
@@ -3,10 +3,16 @@ package com.com.droidcafe;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
+
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,38 @@ public class OrderActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
 
+        radioGroup = findViewById(R.id.radio_group);
+        radioGroup.check(R.id.sameday);
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    // same day service
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    // same day service
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    // same day service
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                // Do nothing
+                break;
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
